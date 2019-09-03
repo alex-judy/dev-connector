7090c0bcf7b7560db692ba875b708346faeb652e
Alex Judy <alex13.judy@gmail.com>
2019-09-03 13:51:56 -0400

    Added client for application

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const config = require('config');
 const request = require('request');
 const { check, validationResult } = require('express-validator/check');
 
@@ -346,9 +345,8 @@ router.get('/github/:username', (req, res) => {
     const options = {
       uri: `https://api.github.com/users/${
         req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
+      }/repos?per_page=5&sort=created:asc&client_id=${process.env.githubClientId
+      }&client_secret=${process.env.githubSecret}`,
       method: 'GET',
       headers: {
         'user-agent': 'node.js'
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator/check");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   GET api/users
 // @desc    Add user
 // @access  Public
 router.post(
-  '/',
+  "/",
   [
-    check('name', 'Name is required.')
+    check("name", "Name is required.")
       .not()
       .isEmpty(),
-    check('email', 'Please include a valid email.').isEmail(),
+    check("email", "Please include a valid email.").isEmail(),
     check(
-      'password',
-      'Please enter a password with 6 or more characters.'
+      "password",
+      "Please enter a password with 6 or more characters."
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
@@ -37,13 +36,13 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'User already exists.' }] });
+          .json({ errors: [{ msg: "User already exists." }] });
       }
 
       const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
+        s: "200",
+        r: "pg",
+        d: "mm"
       });
 
       user = new User({
@@ -67,9 +66,9 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtSecret'),
+        process.env.jwtSecret,
         {
-          expiresIn: config.get('jwtExpiration')
+          expiresIn: process.env.jwtExpiration
         },
         (err, token) => {
           if (err) throw err;
@@ -78,7 +77,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error');
+      res.status(500).send("Server error");
     }
   }
 );
