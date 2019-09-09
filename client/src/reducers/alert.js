40895f44a51515d0442a9327933d5d2d550fba8f
Alex Judy <alex.rjudy@gmail.com>
2019-09-09 16:34:13 -0400

    Adding alert to redux actions

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,18 +1,16 @@
 import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
-const initialState = [
-  {
-    id: 1,
-    msg: "Please login",
-    alertType: "success"
-  }
-];
+const initialState = [];
 
 export default function(state = initialState, action) {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
     case SET_ALERT:
-      return [...state];
+      return [...state, payload];
     case REMOVE_ALERT:
-      return [...state];
+      return state.filter(alert => alert.id !== payload);
+    default:
+      return state;
   }
 }
