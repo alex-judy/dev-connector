690e1b405cdcf814affbf068cb778454a123cb9a
Alex Judy <alex.rjudy@gmail.com>
2019-09-12 16:36:57 -0400

    Redux updates

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth.action";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
-    name: "",
     email: "",
-    password: "",
-    password2: ""
+    password: ""
   });
 
   const { email, password } = formData;
@@ -16,9 +17,12 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-
-    console.log("SUCCESS");
+    login(email, password);
   };
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Fragment>
@@ -59,4 +63,16 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
